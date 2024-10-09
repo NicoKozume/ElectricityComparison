6becf7517e463daf7806479412870e101b4907b0
Nico Kozume <n.struchholtz@gmail.com>
2024-10-09 22:38:43 +0200

    IT WORKS NOOWWWW

--- verivox_tariff_calculator/apps/verivox_tariff_calculator_frontend/src/app/app.component.ts
+++ verivox_tariff_calculator/apps/verivox_tariff_calculator_frontend/src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   standalone: true,
   imports: [NxWelcomeComponent, RouterModule],
+  providers: [HttpClient],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'verivox_tariff_calculator_frontend';
+   constructor(private http: HttpClient){}
+
+   ngOnInit(): void {
+    this.http.get('api/WeatherForecast').subscribe(x => console.info(x));
+   }
+
 }
